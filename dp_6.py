780e252ec83605d7faa9029464d4b69404c68e7a
AryaPS2004 <111893091+AryaPS2004@users.noreply.github.com>
2024-09-14 21:04:49 +0530

    dp_6.py

--- dp_6.py
+++ dp_6.py
@@ -1,3 +1,18 @@
+"""
+Problem : Find All Subarrays with Zero Sum
+You are given an integer array arr of size n. Your task is to find all the subarrays whose elements sum up to zero. A subarray is defined as a contiguous part of the array, and you must return the starting and ending indices of each subarray.
+
+Input :
+An integer array arr of size n where n represents the number of elements in the array.
+Example : 
+Input: [1, 2, -3, 3, -1, 2]
+
+Output :
+- Return a list of tuples, where each tuple contains two integers. The starting index (0-based) of the subarray. The ending index (0-based) of the subarray.
+- The output should list all subarrays that sum to zero. If no such subarrays are found, return an empty list.
+"""
+
+
 def find_subarray(arr):
     sum_map={}
     result=[]
