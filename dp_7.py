735cab75644b90c161385c70bfb0755f26b013e9
AryaPS2004 <111893091+AryaPS2004@users.noreply.github.com>
2024-09-16 23:02:52 +0530

    Add files via upload

---
+++ dp_7.py
@@ -0,0 +1,22 @@
+def trap(height):
+    if not height:
+        return 0
+    
+    l,r=0,len(height)-1
+    leftMax,rightMax=height[l],height[r]
+    res=0
+    
+    while l<r:
+        if leftMax<rightMax:
+            l+=1
+            leftMax=max(leftMax,height[l])
+            res+=leftMax-height[l]
+        else:
+            r-=1
+            rightMax=max(rightMax,height[r])
+            res+=rightMax-height[r]
+            
+    return res
+
+height = list(map(int, input("Enter the heights of bars: ").split()))
+print(f"Total water trapped: {trap(height)}")
